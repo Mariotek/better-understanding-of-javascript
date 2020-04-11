bdad702210d19e3d3917017876988fcf72f9d801
jafarRezaei <bomber.man87@yahoo.com>
2020-04-11 09:59:26 +0430

    fix service worker

--- workbox-config.js
+++ workbox-config.js
@@ -4,7 +4,44 @@ module.exports = {
   swDest: "HTML/sw.js",
   skipWaiting: true,
   clientsClaim: true,
+  runtimeCaching: [
+    {
+      urlPattern: "/",
+      handler: "NetworkFirst",
+      options: {
+        networkTimeoutSeconds: 10,
+        cacheName: "app-cache",
+        expiration: {
+          maxEntries: 5,
+          maxAgeSeconds: 60,
+        },
+        // Configure background sync.
+        backgroundSync: {
+          name: "my-queue-name",
+          options: {
+            maxRetentionTime: 60 * 60,
+          },
+        },
+      },
+    },
+    {
+      // Match any request that ends with .png, .jpg, .jpeg or .svg.
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
 
+      // Apply a cache-first strategy.
+      handler: "CacheFirst",
+
+      options: {
+        // Use a custom cache name.
+        cacheName: "images",
+
+        // Only cache 10 images.
+        expiration: {
+          maxEntries: 20,
+        },
+      },
+    },
+  ],
   // Define runtime caching rules.
   // runtimeCaching: [
   //   {
